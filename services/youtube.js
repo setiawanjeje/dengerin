fa0f4afe7af80b0171940a9dc463266e60b94a08
fawwaz <fawwazmuhammad@gmail.com>
2020-10-12 14:19:37 +0700

    aneh

--- services/youtube.js
+++ services/youtube.js
@@ -8,20 +8,18 @@ const { URL } = require("./urls");
 
 // ("q=olafur%20arnalds&part=id&maxResults=20&type=video&key=AIzaSyBajhSzhKGPfixYYODVAKZ8HUc3NjHcJ-A");
 
+// jukebox : part=id&maxResults=20&type=video&q=olafur%20arnalds&key=AIzaSyBajhSzhKGPfixYYODVAKZ8HUc3NjHcJ-A
+
 // TODO : should store the information in more secure / separate file
 const API_KEY = "AIzaSyCGX5i8Bt-AO7DDcDHLPHIMkiryDGfEeRY";
 
 function searchYT(query) {
   return axios
-    .get(URL.YOUTUBE_SEARCH_URL, {
-      params: {
-        q: encodeURI(query),
-        part: "id",
-        maxResults: 20,
-        type: "video",
-        key: API_KEY,
-      },
-    })
+    .get(
+      `https://content.googleapis.com/youtube/v3/search?part=id&maxResults=20&type=video&q=${encodeURI(
+        query
+      )}&key=${API_KEY}`
+    )
     .then((res) => res.data);
 }
 
